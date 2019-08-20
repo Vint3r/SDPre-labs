3704885fae23cc146d0e4a06836b9a768a62179f
David Norris <Norris.David.Dev@gmail.com>
2019-08-20 15:08:25 -0600

    finding the hypotenuse

--- Objective2Lab5.java
+++ Objective2Lab5.java
@@ -4,8 +4,8 @@ public class Objective2Lab5 {
 
     side1 = 5;
     side2 = 10;
-    hypotenuse = side1 * side1 + side2 * side2;
+    hypotenuse = Math.sqrt(side1 * side1 + side2 * side2);
 
-    System.out.println("The length of the hypotenuse is " + hypotenuse / 2);
+    System.out.println("The length of the hypotenuse is " + hypotenuse);
   }
 }
