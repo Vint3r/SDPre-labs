4ab65ce261351f51fbfad034f34ba01d5c7e535b
David Norris <Norris.David.Dev@gmail.com>
2019-08-20 14:45:07 -0600

    calculating averages

---
+++ Objective2Lab4.java
@@ -0,0 +1,12 @@
+public class Objective2Lab4 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 6;
+    num2 = 10;
+    num3 = 27;
+    average = num1 + num2 + num3 / 3;
+
+    System.out.println("The average of the 3 variables is " + average);
+  }
+}
