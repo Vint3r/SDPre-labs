9b2fa6d261782669015f4e9c3d1766ee7d1250f5
David Norris <Norris.David.Dev@gmail.com>
2019-08-20 14:08:59 -0600

    My bio

---
+++ Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "David Norris";
+    int age = 25;
+    String state = "Colorado";
+    System.out.println("My name is " + name + ". I am " + age + " years old, born and raised in the state of " + state + ".");
+  }
+}
